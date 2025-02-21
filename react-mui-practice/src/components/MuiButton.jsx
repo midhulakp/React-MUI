c7698caaa4563b334bdff8ff13cf5e31f26c12d7
Midhula K P <kpmidhula7@gmail.com>
2025-02-21 20:30:48 +0530

    add mui button component

---
+++ react-mui-practice/src/components/MuiButton.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import { useState } from "react";
+
+const MuiButton = () => {
+  const [formats, setFormats] = useState([null]);
+  console.log(formats);
+  const handleFormatChange = (_event, updatedFormats) => {
+    setFormats(updatedFormats);
+  };
+
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" color="primary">
+          Primary
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="outlined" color="info">
+          Info
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="text" color="primary">
+          Primary
+        </Button>
+        <Button variant="text" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="text" color="warning">
+          Warning
+        </Button>
+        <Button variant="text" color="info">
+          Info
+        </Button>
+        <Button variant="text" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("clicked")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="warning" size="medium">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+
+      <Stack direction="row">
+        <ButtonGroup variant="contained">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+
+      <Stack direction="row">
+        <ButtonGroup variant="outlined">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+
+      <Stack direction="row">
+        <ButtonGroup
+          variant="text"
+          orientation="vertical"
+          size="small"
+          color="success"
+        >
+          <Button onClick={() => alert("left clicked")}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+
+      <Stack direction="row">
+        <ToggleButtonGroup
+          aria-label="text formatting"
+          value={formats}
+          onChange={handleFormatChange}
+          size="small"
+          color="secondary"
+          exclusive
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
